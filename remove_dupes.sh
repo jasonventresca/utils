fa79deaca6c66c3e7554a12c8ada41b1b66725fd
Jason Ventresca <jasonventresca@gmail.com>
2013-03-01 18:53:38 +0000

    added remove_dupes tool

---
+++ remove_dupes.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# @brief:  toggle between two versions of a file (suffixed with .$extension_A and.$extension_B)
+# @usage:  ./remove_dupes.sh input_file output_file
+set -eux
+
+input_file=$1
+output_file=$2
+
+sort input_file | uniq -d > output_file && sort input_file | uniq -u >> output_file
+
